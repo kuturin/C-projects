22101d8c871605d0e957d02552e102fd37470677
kuturin <turkurun2@gmail.com>
2025-03-05 00:12:01 +0100

    Added miniprogram that print every multiple of 17 in chosen number

---
+++ AllMultiplesOf17/AllMultiplesOf17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllMultiplesOf17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine()), a, i;
+            for (i = 1; i <= number; i++)
+            {
+                a = i % 17;
+                if (a == 0) 
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ SumOfAllTheMultiplesOf3And5/SumOfAllTheMultiplesOf3And5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfAllTheMultiplesOf3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number to find the sum of all the multiples of 3 and 5 in the number: ");
+            int a, b, i, sum = 0, sumNumber = int.Parse(Console.ReadLine());
+            
+            for (i = 1; i<= sumNumber; i++) 
+            {
+                a = i % 3;
+                b = i % 5;
+                if (a == 0 || b == 0)
+                {
+                    sum = sum + i;
+                }
+
+            }
+            Console.WriteLine("The sum of all the multiples of 3 and 5 in " + sumNumber + " is: " + sum);
+        }
+    }
+}

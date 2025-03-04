77bc57cbd2434f347f1480a0283b3ad1e15eda8a
kuturin <turkurun2@gmail.com>
2025-03-04 19:57:54 +0100

    Dodaj pliki projektów.

---
+++ IsDivisibleBy2/IsDivisibleBy2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsDivisibleBy2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            if ( number % 2 == 0)
+            {
+                Console.WriteLine(number + " is divisible by 2");
+            }
+            else
+            {
+                Console.WriteLine(number + " isn't divisible by 2");
+            }
+        }
+    }
+}
---
+++ Odd or even/Odd or even/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_or_even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+                Console.Read();
+            }
+        }
+    }
+}

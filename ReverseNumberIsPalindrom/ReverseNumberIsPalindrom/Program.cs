3987d76a3892492a70dc95920a922699c44fad88
kuturin <turkurun2@gmail.com>
2025-03-05 20:12:05 +0100

    Added mini program that check if chosen number is palindrom or not

---
+++ ReverseNumberIsPalindrom/ReverseNumberIsPalindrom/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumberIsPalindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string stringNumber = Console.ReadLine();
+            int pos = 0;
+            string reversedStringNumber = "";
+            
+
+            for (int i = stringNumber.Length - 1; i >= 0; i--)
+            {
+                reversedStringNumber += stringNumber[i];
+            }
+            int reversedNumber = int.Parse(reversedStringNumber);
+            int number = int.Parse(stringNumber);
+            Console.WriteLine("The reversed " + number + " is: " + reversedNumber);
+            if (reversedNumber == number)
+            {
+                Console.WriteLine("The number is a palindrom");
+            }
+            else
+            {
+                Console.WriteLine("The number isn't a palindrom");
+            }
+        }
+    }
+}

423db5d7e6954c368589d23a44acdff40db8bb64
kuturin <turkurun2@gmail.com>
2025-03-06 14:35:04 +0100

    Added miniprogram that convert 2 chosen numbers to binary numbers and add them

---
+++ SumOfToBinaryNumbers/SumOfToBinaryNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfToBinaryNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            string firstBinaryString = Convert.ToString(firstNumber, 2);
+            Console.WriteLine("First number in binary is: " + firstBinaryString);
+            string secondBinaryString = Convert.ToString(secondNumber, 2);
+            Console.WriteLine("Second number in binary is: " + secondBinaryString);
+
+            int firstBinary = Convert.ToInt32(firstBinaryString, 2);
+            int secondBinary = Convert.ToInt32(secondBinaryString, 2);
+            string sum = Convert.ToString(firstBinary + secondBinary, 2);
+            Console.WriteLine("Sum of two numbers in binary is: " + sum);
+        }
+    }
+}

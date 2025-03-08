475392d90a2e0ede1b3afc07eeddf8c6520c4f09
kuturin <turkurun2@gmail.com>
2025-03-08 10:46:04 +0100

    Add miniprogram that performs multiplication of exponents of same base and then show result of it

---
+++ ExponentsCalculator/ExponentsCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExponentsCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the base number: ");
+            double baseNumber = double.Parse(Console.ReadLine());
+            Console.Write("Enter the first exponent: ");
+            double fisrtExponent = double.Parse(Console.ReadLine());
+            Console.Write("Enter the second exponent: ");
+            double secondExponent = double.Parse(Console.ReadLine());
+
+            double mul = fisrtExponent + secondExponent;
+
+            double result = Math.Pow(baseNumber, mul);
+
+            Console.WriteLine("The result of " + baseNumber + " raised to the power of " + fisrtExponent + " and " + secondExponent + " is: " + baseNumber + " raised to the power of " + mul + " which is: " + result);
+        }
+    }
+}

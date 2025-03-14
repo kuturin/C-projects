ca0b28ae5a616e484096284fce5a740e844e8c44
kuturin <turkurun2@gmail.com>
2025-03-14 18:59:36 +0100

    Added program that let user input his age and legal age in his country, program will check if they are adult (his age is at least legal age)

---
+++ isAdult/isAdult/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isAdult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string legalInput;
+            Console.Write("Enter your age:");
+            input = Console.ReadLine();
+            while (!int.TryParse(input, out int inputAge) || (int.Parse(input)) < 0 || (int.Parse(input)) > 150)
+            {
+                Console.WriteLine("Please enter a valid number.");
+                input = Console.ReadLine();
+            }
+            int age = int.Parse(input);
+            Console.Write("Enter legal age in your country:");
+            legalInput = Console.ReadLine();
+            while (!int.TryParse(legalInput, out int inputLegalAge) || (int.Parse(legalInput)) < 12  || (int.Parse(legalInput)) > 25)
+            {
+                Console.WriteLine("Please enter a valid number.");
+                legalInput = Console.ReadLine();
+            }
+            int legalAge = int.Parse(legalInput);
+            if (age >= legalAge)
+            {
+                Console.WriteLine("You are an adult.");
+            }
+            else
+            {
+                Console.WriteLine("You are not an adult.");
+            }
+        }
+    }
+}

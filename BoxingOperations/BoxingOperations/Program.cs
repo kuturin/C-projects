4960f18258e0c92759a15bcee36fbf8dcb03c027
kuturin <turkurun2@gmail.com>
2025-03-15 21:57:50 +0100

    Added program to demonstrate boxing operations in C#

---
+++ BoxingOperations/BoxingOperations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingOperations
+{
+    class sample
+    {
+        int x = 10;
+        object obj;
+        void boxmethod()
+        {
+            sample s = new sample();
+            bool b;
+            object ob = "CSHARP";
+            b = s.obj is int;
+            Console.WriteLine(b);
+            s.obj = x;
+            b = s.obj is int;
+            Console.WriteLine("{0},{1},{2}", s.obj,s.x,b);
+            s.x = (int)s.obj;
+            s.x = 20;
+            b = s.obj is int;
+            Console.WriteLine("{0},{1},{2}", s.obj, s.x, b);
+            s.obj = "CSHARP";
+            b = s.obj is int;
+            Console.WriteLine("{0},{1},{2}", s.obj, s.x, b);
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            sample s = new sample();
+            s.boxmethod();
+        }
+    }
+}

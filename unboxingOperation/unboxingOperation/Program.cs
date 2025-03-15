f4373afe62f3ba74fee2069d7cd20cf7d4a286bd
kuturin <turkurun2@gmail.com>
2025-03-15 22:03:38 +0100

    Added program to demonstrate unboxing operation in c#

---
+++ unboxingOperation/unboxingOperation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unboxingOperation
+{
+    class sample
+    {
+        int data;
+        void insert(object x)
+        {
+            data = (int)x * 5;
+        }
+        object delete()
+        {
+            data = 0;
+            return (object)data;
+        }
+        static void Main(string[] args)
+        {
+            sample s = new sample();
+            s.insert(10);
+            Console.WriteLine("Data: {0}", s.data);
+            Console.WriteLine("Data: {0}", s.delete());
+            Console.ReadLine();
+        }
+    }
+}

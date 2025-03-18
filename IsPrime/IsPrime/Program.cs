236c45c9ad15125f35ccf9265af04faa54a9c02e
kuturin <turkurun2@gmail.com>
2025-03-18 18:55:10 +0100

    Added miniprogram that print all prime numbers  in range from 0 to chosen number

---
+++ IsPrime/IsPrime/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a last number: ");
+            string input = Console.ReadLine();
+            bool isNumber = int.TryParse(input, out int value);
+            while (!isNumber)
+            {
+                Console.WriteLine("Write valid number");
+                input = Console.ReadLine();
+                isNumber = int.TryParse(input, out value);
+            }
+            int range = int.Parse(input);
+            bool isPrime = true;
+            Console.WriteLine($"Prime numbers from 0 to {range} are: ");
+            for (int i = 2; i <= range; i++)
+            {
+                for (int j = 2; j <= range; j++)
+                {
+                    if (i != j && (i % j == 0))
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) 
+                {
+                    Console.WriteLine(i);
+                }
+                isPrime = true;
+            }
+
+
+
+        }
+    }
+}

de45cac09afebdb0a2c86cd4e51d744581175212
kuturin <turkurun2@gmail.com>
2025-03-22 13:34:05 +0100

    Added program that sum numbers from 0 to n

---
+++ SumOfNNumbers/SumOfNNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfNNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            bool isNumber = int.TryParse(input, out int number);
+            while (!isNumber)
+            {
+                Console.WriteLine("Invalid input. Please enter a number");
+                input = Console.ReadLine();
+                isNumber = int.TryParse(input, out number);
+            }
+            int n = int.Parse(input);
+            int sum = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"The sum of numbers from 0 to {n} is {sum}");
+        }
+    }
+}

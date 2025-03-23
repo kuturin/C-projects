e5741e4f1dcfc0c4e5e046031226e7c0ca3e8f40
kuturin <turkurun2@gmail.com>
2025-03-23 20:00:27 +0100

    Added program add sum of natural numbers amount which user entered

---
+++ SumOfNaturalNumbers/SumOfNaturalNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfNaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter how much natural numbers to sum?");
+            string input = Console.ReadLine();
+            bool isNumber = int.TryParse(input, out int number);
+            while (!isNumber)
+            {
+                Console.WriteLine("Please enter a valid number");
+                input = Console.ReadLine();
+                isNumber = int.TryParse(input, out number);
+            }
+            int n = int.Parse(input);
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"Sum of first {n} natural numbers is {sum}");
+        }
+    }
+}

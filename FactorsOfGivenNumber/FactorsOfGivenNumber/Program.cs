05f4386152fe4cc8578951bddb6e617f89dc3784
kuturin <turkurun2@gmail.com>
2025-03-24 18:56:01 +0100

    Added program that print all factors of given number

---
+++ FactorsOfGivenNumber/FactorsOfGivenNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorsOfGivenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            while (!int.TryParse(input, out int number))
+            {
+                Console.Write("Enter a valid number: ");
+                input = Console.ReadLine();
+            }
+            int num = int.Parse(input);
+            Console.WriteLine("Factors of " + num + " are:");
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.Write(i + ", ");
+                }
+            }
+        }
+    }
+}

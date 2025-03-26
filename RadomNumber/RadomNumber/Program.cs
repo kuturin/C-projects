a4cee13b75491ca4ac95a802131dea8a09f667e9
kuturin <turkurun2@gmail.com>
2025-03-26 20:22:41 +0100

    Added program that generate random number in range from 1 to given number

---
+++ RadomNumber/RadomNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadomNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the max range of random number: ");
+            string input = Console.ReadLine();
+            while (!int.TryParse(input, out int n))
+            {
+                Console.Write("Invalid input. Please enter a valid number: ");
+                input = Console.ReadLine();
+            }
+            int number = int.Parse(input);
+            Random random = new Random();
+            Console.WriteLine(random.Next(1, number));
+        }
+    }
+}

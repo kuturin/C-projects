09650da22b7cde9982dd1dd9813ff443c22cf3b5
kuturin <turkurun2@gmail.com>
2025-03-28 20:19:56 +0100

    Added program that print square root of given number

---
+++ SquareRoot/SquareRoot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            while (!double.TryParse(input, out double number))
+            {
+                Console.Write("Invalid input. Please enter a number: ");
+                input = Console.ReadLine();
+            }
+            double num = double.Parse(input);
+            double result = Math.Sqrt(num);
+            Console.WriteLine($"The square root of {num} is {result}");
+        }
+    }
+}

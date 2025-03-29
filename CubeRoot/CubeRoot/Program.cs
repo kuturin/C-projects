a198c5ae60a7c7c8a30e46ec1a66058703c76bb5
kuturin <turkurun2@gmail.com>
2025-03-29 15:19:42 +0100

    Added program that find cube root of entered number

---
+++ CubeRoot/CubeRoot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubeRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to find the cube root of: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            double cubeRoot = Math.Pow(num, 1.0 / 3.0);
+            Console.WriteLine("The cube root of " + num + " is " + cubeRoot);
+        }
+    }
+}

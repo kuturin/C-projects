1b7f7d467d1180d0d40f2e464d373c4e743cb020
kuturin <turkurun2@gmail.com>
2025-03-31 20:47:24 +0200

    Added program that raise given number to the power of given number

---
+++ PowerOf/PowerOf/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOf
+{
+    class Program
+    {
+        static double input(string input)
+        {
+            double number;
+            string inputString = input;
+            while (!double.TryParse(inputString, out number))
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+                inputString = Console.ReadLine();
+            }
+            number = double.Parse(inputString);
+            return number;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double number = input(Console.ReadLine());
+            Console.Write("Enter the power: ");
+            double power = input(Console.ReadLine());
+            double result = Math.Pow(number, power);
+            Console.WriteLine($"{number} raised to the power of {power} is {result}");
+
+
+
+        }
+    }
+}

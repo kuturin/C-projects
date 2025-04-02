b0f05e4aa91a5e8e3fd9902804152f91bb063540
kuturin <turkurun2@gmail.com>
2025-04-02 16:48:39 +0200

    Add program that shows local time

---
+++ GetLocalTime/GetLocalTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetLocalTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeZone zone = TimeZone.CurrentTimeZone;
+            DateTime local = zone.ToLocalTime(DateTime.Now);
+            Console.WriteLine("Local time: {0}", local);
+        }
+    }
+}

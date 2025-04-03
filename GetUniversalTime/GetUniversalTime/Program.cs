9835db2ab99eada93a3056b5ff3a7f42cc0f9411
kuturin <turkurun2@gmail.com>
2025-04-03 19:43:59 +0200

    Added program that gives universal time

---
+++ GetUniversalTime/GetUniversalTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetUniversalTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeZone zone = TimeZone.CurrentTimeZone;
+            DateTime univ = zone.ToUniversalTime(DateTime.Now);
+            Console.WriteLine($"Universal Time is {univ}");
+        }
+    }
+}

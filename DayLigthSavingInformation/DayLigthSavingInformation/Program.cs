080b8d4da188fe7ed2c033930ae497102d14f313
kuturin <turkurun2@gmail.com>
2025-04-08 21:47:46 +0200

    Added program that get the DayLight saving information

---
+++ DayLigthSavingInformation/DayLigthSavingInformation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayLigthSavingInformation
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            TimeZone z = TimeZone.CurrentTimeZone;
+            DaylightTime t = z.GetDaylightChanges(DateTime.Now.Year);
+            Console.WriteLine($"Start time: {t.Start}");
+            Console.WriteLine($"Start time: {t.Delta}");
+            Console.WriteLine($"Start time: {t.End}");
+        }
+    }
+}
---
+++ DayLigthSavingInformation/DayLigthSavingInformation/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("DayLigthSavingInformation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DayLigthSavingInformation")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("8b74597c-ce12-4f6f-afd6-f40198e50cdd")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

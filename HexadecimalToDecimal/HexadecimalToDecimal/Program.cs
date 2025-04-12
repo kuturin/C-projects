1ed05ee35a9c383e66cdb8e9cd1994a8769e990c
kuturin <turkurun2@gmail.com>
2025-04-12 19:33:33 +0200

    Added program that convert given hexadecimal number to decimal

---
+++ HexadecimalToDecimal/HexadecimalToDecimal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexadecimalToDecimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter hexadecimal number: ");
+            string input = Console.ReadLine();
+            int num = int.Parse(input, System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine($"Decimal equivalent: {num}");
+        }
+    }
+}

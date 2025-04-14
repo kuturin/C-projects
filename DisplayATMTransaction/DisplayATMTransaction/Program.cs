1d40580443f54ee00dc6dee319f5887a8c2a1d51
kuturin <turkurun2@gmail.com>
2025-04-14 21:30:39 +0200

    Added program that simulate ATM transaction

---
+++ DisplayATMTransaction/DisplayATMTransaction/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayATMTransaction
+{
+    internal class Program
+    {
+        static int isInt(string str)
+        {
+            int num;
+            while (!int.TryParse(str, out num))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                str = Console.ReadLine();
+            }
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            int amount = 1000, deposit, withdraw;
+            int choice, pin = 0, x = 0;
+            Console.Write("Enter your PIN number: ");
+            pin = isInt(Console.ReadLine());
+            while (true)
+            {
+                Console.WriteLine("********Welcome to ATM Service**************\n");
+                Console.WriteLine("1. Check Balance\n");
+                Console.WriteLine("2. Withdraw Cash\n");
+                Console.WriteLine("3. Deposit Cash\n");
+                Console.WriteLine("4. Quit\n");
+                Console.WriteLine("*********************************************\n\n");
+                Console.WriteLine("Enter your choice: ");
+                choice = isInt(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("\n YOUR BALANCE IN Rs : {0} ", amount);
+                        break;
+                    case 2:
+                        Console.WriteLine("\n ENTER THE AMOUNT TO WITHDRAW: ");
+                        withdraw = int.Parse(Console.ReadLine());
+                        if (withdraw % 100 != 0)
+                        {
+                            Console.WriteLine("\n PLEASE ENTER THE AMOUNT IN MULTIPLES OF 100");
+                        }
+                        else if (withdraw > (amount - 500))
+                        {
+                            Console.WriteLine("\n INSUFFICENT BALANCE");
+                        }
+                        else
+                        {
+                            amount = amount - withdraw;
+                            Console.WriteLine("\n\n PLEASE COLLECT CASH");
+                            Console.WriteLine("\n YOUR CURRENT BALANCE IS {0}", amount);
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("\n ENTER THE AMOUNT TO DEPOSIT");
+                        deposit = int.Parse(Console.ReadLine());
+                        amount = amount + deposit;
+                        Console.WriteLine("YOUR BALANCE IS {0}", amount);
+                        break;
+                    case 4:
+                        Console.WriteLine("\n THANK U USING ATM");
+                        break;
+                }
+            }
+            
+
+
+        }
+    }
+}
---
+++ DisplayATMTransaction/DisplayATMTransaction/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("DisplayATMTransaction")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DisplayATMTransaction")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("ef47bf90-7862-477c-aef0-96c43e71f7a0")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

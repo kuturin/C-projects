e7a1f772e49c72222e75946053436079162aa44d
kuturin <turkurun2@gmail.com>
2025-04-15 21:43:08 +0200

    Added program that print IP address

---
+++ GetIPAddress/GetIPAddress/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetIPAddress
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strHostName = string.Empty;
+            strHostName = Dns.GetHostName();
+            Console.WriteLine("Local machine's host name: " + strHostName);
+            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+            IPAddress[] addr = ipEntry.AddressList;
+            for (int i = 0; i < addr.Length; i++)
+            {
+                Console.WriteLine("IP Address " + (i + 1) + ": " + addr[i].ToString());
+            }
+        }
+    }
+}
